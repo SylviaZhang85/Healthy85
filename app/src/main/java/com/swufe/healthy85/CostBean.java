2735d6a1b9b5339983240682cc072410a97c5ddc
SylviaZhang85 <245993268@qq.com>
2020-06-02 16:35:47 +0800

    first

---
+++ app/src/main/java/com/swufe/healthy85/CostBean.java
@@ -0,0 +1,11 @@
+package com.swufe.healthy85;
+
+import java.io.Serializable;
+
+
+    public class CostBean implements Serializable {
+        public String costTitle;
+        public String costDate;
+        public String costMoney;
+    }
+
